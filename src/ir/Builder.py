44bd3ab621c69ae56ea537dd3dd3b3417cd58e16
wangziwenhk <wangziwen2011@icloud.com>
2024-09-12 16:17:25 +0800

    Builder Add CreateFunction

--- src/ir/Builder.py
+++ src/ir/Builder.py
@@ -1,10 +1,29 @@
 import Type
 from llvmlite import ir
 
+
 class Builder:
-    def __init__(self,module:ir.Module):
+    def __init__(self, module: ir.Module):
         self.module = module
         self.context = module.context
+        self.builder = None
 
-    def getModule(self):
+    def getModule(self) -> ir.Module:
         return self.module
+
+    def getContext(self) -> ir.Context:
+        return self.context
+
+    def CreateFunction(self, name: str, return_type: ir.types, args=None):
+        if args is None:
+            args = []
+
+        # Create
+        func_type = ir.FunctionType(return_type, args)
+        function = ir.Function(self.module, func_type, name)
+
+        block = function.append_basic_block('entry')
+        self.builder = ir.IRBuilder(block)
+
+    def CreateAdd(self, x: ir.Value, y: ir.Value) -> ir.Value:
+        return self.builder.CreateAdd(x, y)
